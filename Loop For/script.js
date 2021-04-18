1ad2a65877b8b0035a07691fd263e33bc86965a2
messiashub <claudineimessias@hotmail.com>
2021-04-18 02:07:00 -0300

    update

---
+++ Loop For/script.js
@@ -0,0 +1,10 @@
+// FOR LOOP
+// FOR LOOP PARA ARRAY
+
+let texto = '';
+
+for (let i = 0; i <= 10; i++) {
+    texto = texto + i + '<br/>';
+
+};
+document.getElementById('demo').innerHTML = texto;
