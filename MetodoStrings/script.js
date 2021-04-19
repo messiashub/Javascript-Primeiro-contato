e9ebdf454a037cdef5e8c9d54cfce5b8e6604450
messiashub <claudineimessias@hotmail.com>
2021-04-19 00:24:28 -0300

    UPDATE

---
+++ MetodoStrings/script.js
@@ -0,0 +1,24 @@
+
+// toUpperCase -> converte string em MAIÚSCULA
+
+let nome = 'claudinei messias';
+
+console.log (`Convertendo em maiúscula o valor da variável nome = ${nome.toUpperCase()}`);
+
+////////////////////////////
+
+// toLowerCase -> converte string em minúsculas
+let sobrenome = 'MessiaS'
+
+console.log(`Convertendo em minúsculas o valor da variável sobrenome = ${sobrenome.toLowerCase()}`);
+
+///////////////////////////
+
+// String.trim() -> O método remove o espaço branco de ambos os lados de uma string:trim()
+let espace = '  oval   ';
+
+console.log(espace);
+console.log(espace.trim());
+
+///////////////////////////////////////////
+
