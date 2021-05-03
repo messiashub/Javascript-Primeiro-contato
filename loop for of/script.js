40386e3a2896a77e9d4e55f9f245012896851416
messiashub <claudineimessias@hotmail.com>
2021-05-03 00:30:15 -0300

    update

--- loop for of/script.js
+++ loop for of/script.js
@@ -6,20 +6,31 @@ for (variable of iterable) {
     // code block to be executed
   }
 */
+let notas = [10,35,80,95,100,5,66,77,15];
+let aprovado = 0;
+let reprovado = 0;
 
+for(let nota of notas){
+    if(nota >= 50){                   // se nota for maior que 50 , aprovado recebe + 1
+        aprovado++
+    }else{                            // se não , reprovado recebe + 1 
+        reprovado++
+    }  
+}
+console.log(`aprovados: ${aprovado}`);
+console.log(`aprovados: ${reprovado}`);
+//////////////////////////////
 let numeros = [100, 99, 89];
 
-for(let numero of numeros){
-    let dobro = numero * 2
+for(let numero of numeros){ 
+    let dobro = numero * 2    // iterando sobre os valores e multiplicando por 2
     
     console.log(dobro);
 }
-//onsole.log(numero);
-
-
+///////////////////////////
 let nome = 'Claudinei'
 
 for(let n of nome){
-    let upper = n.toUpperCase()
+    let upper = n.toUpperCase()  // iterando sobre string e  aplicando caixaAlta
     console.log(upper);
 }
