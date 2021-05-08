5b21afff3bb0aa33a57667c3261ff87921986ae6
messiashub <claudineimessias@hotmail.com>
2021-05-08 15:43:51 -0300

    update

--- Função Callback/script.js
+++ Função Callback/script.js
@@ -2,6 +2,7 @@
 // FUNÇÃO CALLBACK 
 
 // De forma simples, callback é uma função passada como parâmetro para outra função.
+// Basicamente, callback é um tipo de função que só é executada após o processamento de outra função. Na linguagem JavaScript, quando uma função é passada como um argumento de outra, ela é, então, chamada de callback.
 
 function alertar(){
     console.log('Opa, tudo bem?');
@@ -12,3 +13,29 @@ setTimeout(alertar, 2000);
 let sobrenome = 'Messias';
 
 console.log(`Nome Completo: ${nome} ${sobrenome}`)
+
+//////////////////
+
+function soma(a,b, exibir){
+    let op = a + b;
+    exibir(op);
+}
+
+function multiplica(a,b, exibir){
+    let op = a * b;
+    exibir(op);
+}
+
+function exibir(op){
+    console.log(`A operação resulta em ${op}`)
+}
+
+soma(2,5, exibir);
+multiplica(3,3, exibir);
+
+///////////////////////////////////
+
+const $botao = document.querySelector('button');
+$botao.addEventListener('click',()=>{
+    console.log('botao clicado')
+});
